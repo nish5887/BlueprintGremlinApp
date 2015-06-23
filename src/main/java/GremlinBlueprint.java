585163dceccb91be0343b4545ba73f30c42bc63f
Nishita Rao <nishitarao@ITs-MacBook-Air-14.local>
2015-06-23 11:41:04 -0700

    Adding the check to see if already follows, changed the graph for testing

--- src/main/java/GremlinBlueprint.java
+++ src/main/java/GremlinBlueprint.java
@@ -15,21 +15,22 @@ public class GremlinBlueprint {
     static Vertex person1,person2,person3,person4,person5,person6,restaurant1,restaurant2,restaurant3,restaurant4,restaurant5,restaurant6;
     static Edge link1,link2,link3,link4,link5;
 
-
+    static Graph graph = GraphFactory.open("resources/graph.properties");
+    
     //Creates a graph with 6 people and 6 restaurants, and creates a few edges
     public static void CreateGraph(){
-        person1 = GlobalVars.graph.addVertex(1);
-        person2 = GlobalVars.graph.addVertex(2);
-        person3 = GlobalVars.graph.addVertex(3);
-        person4 = GlobalVars.graph.addVertex(4);
-        person5 = GlobalVars.graph.addVertex(5);
-        person6 = GlobalVars.graph.addVertex(6);
-        restaurant1 = GlobalVars.graph.addVertex(7);
-        restaurant2 = GlobalVars.graph.addVertex(8);
-        restaurant3 = GlobalVars.graph.addVertex(9);
-        restaurant4 = GlobalVars.graph.addVertex(10);
-        restaurant5 = GlobalVars.graph.addVertex(11);
-        restaurant6 = GlobalVars.graph.addVertex(12);
+        person1 = graph.addVertex(1);
+        person2 = graph.addVertex(2);
+        person3 = graph.addVertex(3);
+        person4 = graph.addVertex(4);
+        person5 = graph.addVertex(5);
+        person6 = graph.addVertex(6);
+        restaurant1 = graph.addVertex(7);
+        restaurant2 = graph.addVertex(8);
+        restaurant3 = graph.addVertex(9);
+        restaurant4 = graph.addVertex(10);
+        restaurant5 = graph.addVertex(11);
+        restaurant6 = graph.addVertex(12);
 
         person1.setProperty("name","A");
         person2.setProperty("name","B");
@@ -45,12 +46,12 @@ public class GremlinBlueprint {
         restaurant6.setProperty("name","R6");
 
 
-        link1 = GlobalVars.graph.addEdge(null, person1, restaurant1, "Visits");
-        link1 = GlobalVars.graph.addEdge(null, person1, restaurant2, "Visits");
-        link2 = GlobalVars.graph.addEdge(null, person2, restaurant1, "Visits");
-        link3 = GlobalVars.graph.addEdge(null, person3, restaurant1, "Visits");
-        link4 = GlobalVars.graph.addEdge(null, person2, person1, "Follows");
-        link5 = GlobalVars.graph.addEdge(null, person3, person2, "FollowedBy");
+        link1 = graph.addEdge(null, person1, restaurant1, "Visits");
+        link1 = graph.addEdge(null, person1, restaurant2, "Visits");
+        link2 = graph.addEdge(null, person4, restaurant1, "Visits");
+        link3 = graph.addEdge(null, person6, restaurant2, "Visits");
+        link4 = graph.addEdge(null, person2, person1, "Follows");
+        link5 = graph.addEdge(null, person3, person2, "Follows");
 
 
     }
@@ -61,22 +62,23 @@ public class GremlinBlueprint {
         // It depends on 1) People who visit the same restaurant as him/her 2) People who they follow follow someone else
 
         List<String> AlreadyFollowing = new ArrayList<String>();
-        GremlinPipeline GraphForPeopleAlreadyBeingFollowed = new GremlinPipeline(GlobalVars.graph);
+        GremlinPipeline GraphForPeopleAlreadyBeingFollowed = new GremlinPipeline(graph);
         GraphForPeopleAlreadyBeingFollowed.V("name", namePerson).out("Follows");
         for (Object nameFollowed: GraphForPeopleAlreadyBeingFollowed.property("name")){
             AlreadyFollowing.add(nameFollowed.toString());
 
         }
 
-        GremlinPipeline GraphPipeForRestaurantsVisitors = new GremlinPipeline(GlobalVars.graph);
+        GremlinPipeline GraphPipeForRestaurantsVisitors = new GremlinPipeline(graph);
 //        pipe.V("name", "John");
         GraphPipeForRestaurantsVisitors.V("name",namePerson).out("Visits");
 
         for (Object nameRes :GraphPipeForRestaurantsVisitors.property("name")) {
             System.out.println(nameRes); //prints the restaurant name the person visited
-            GremlinPipeline VisitsPipe = new GremlinPipeline(GlobalVars.graph).V("name", (String) nameRes).in("Visits");
+            GremlinPipeline VisitsPipe = new GremlinPipeline(graph).V("name", (String) nameRes).in("Visits");
             for (Object nameP : VisitsPipe.property("name")) {
                 if(!nameP.equals(namePerson) && !AlreadyFollowing.contains(nameP.toString())){
+                    AlreadyFollowing.add(nameP.toString());
                     System.out.println(namePerson + " should follow " + nameP);}
             }
 
@@ -84,14 +86,15 @@ public class GremlinBlueprint {
 
 
 
-        GremlinPipeline GraphPipeForFollowed = new GremlinPipeline(GlobalVars.graph);
+        GremlinPipeline GraphPipeForFollowed = new GremlinPipeline(graph);
         GraphPipeForFollowed.V("name", namePerson).out("Follows");
 
         for (Object nameFollows: GraphPipeForFollowed.property("name")) {
             System.out.println(nameFollows); //prints the person name who is following namePerson
-            GremlinPipeline FollowsPipe = new GremlinPipeline(GlobalVars.graph).V("name", (String) nameFollows).in("Follows");
+            GremlinPipeline FollowsPipe = new GremlinPipeline(graph).V("name", (String) nameFollows).out("Follows");
             for (Object nameP : FollowsPipe.property("name")) {
                 if(!nameP.equals(namePerson) && !AlreadyFollowing.contains(nameP.toString()) ){
+                    AlreadyFollowing.add(nameP.toString());
                     System.out.println(namePerson + " should follow " + nameP);}
             }
 
@@ -100,10 +103,10 @@ public class GremlinBlueprint {
 
     }
     public static void main( String[] args ) throws ScriptException {
-        String name = "A";
-        //CreateGraph();
+        String name = "C";
+        CreateGraph();
         TraverseGraph(name);
-        GlobalVars.graph.shutdown();
+        graph.shutdown();
 
     }
 
